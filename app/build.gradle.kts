05f076d466dfac6f4af62dfa3c8a409a1620d806
82102 <nidus0526@gmail.com>
2024-01-15 12:56:26 +0900

    1. AddHappyPlace UI 화면 만드는 작업 중

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.backend.wasm.ir2wasm.bind
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -5,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.happyplace"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.happyplace"
@@ -32,6 +34,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+
+    buildFeatures{
+        viewBinding = true
     }
 }
 
--- app/src/main/java/com/example/happyplace/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplace/AddHappyPlaceActivity.kt
@@ -2,10 +2,24 @@ package com.example.happyplace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.happyplace.databinding.ActivityAddHappyPlaceBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddHappyPlaceBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbarAddPlace)
+        if(supportActionBar != null) {
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowTitleEnabled(true)
+            binding.toolbarAddPlace.title = "ADD HAPPY PLACE"
+        }
+
+        binding.toolbarAddPlace.setNavigationOnClickListener{
+            finish()
+        }
     }
 }
