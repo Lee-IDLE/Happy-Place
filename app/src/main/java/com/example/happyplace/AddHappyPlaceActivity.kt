cf5e031c908dfa57f37a4e840451ba982aee8060
Lee-IDLE <nidus0526@gmail.com>
2024-01-24 10:27:57 +0900

    1. Dexter 라이브러리 사용 추가(권한 가져오기 관련 라이브러리) 2. 권한이 없을 시 앱의 '설정' 화면을 띄워서 권한 설정을 하도록 하는 기능 추가

--- app/src/main/java/com/example/happyplace/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplace/AddHappyPlaceActivity.kt
@@ -1,11 +1,24 @@
 package com.example.happyplace
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.DatePicker
+import android.widget.Toast
 import com.example.happyplace.databinding.ActivityAddHappyPlaceBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -37,7 +50,8 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
             cal.set(Calendar.DAY_OF_YEAR, dayOfMonth)
             updateDateInView()
         }
-        binding.etDate.setOnClickListener(this)
+        binding.etDate.setOnClickListener(this@AddHappyPlaceActivity)
+        binding.tvAddImage.setOnClickListener(this@AddHappyPlaceActivity)
     }
 
     override fun onClick(v: View?) {
@@ -45,9 +59,75 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
             R.id.etDate -> {
                 DatePickerDialog(this@AddHappyPlaceActivity, dateSetListener,
                     cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_YEAR)).show()
+            }
+            R.id.tvAddImage -> {
+                val pictureDialog = AlertDialog.Builder(this@AddHappyPlaceActivity)
+                pictureDialog.setTitle("Select Action")
+                val pictureDialogItems = arrayOf("Select photo from Gallery",
+                    "Capture photo from camera")
+                pictureDialog.setItems(pictureDialogItems){ dialog, which ->
+                    when(which){
+                        0 -> {choosePhotoFromGallery()}
+                        1 -> {
+                            Toast.makeText(this@AddHappyPlaceActivity,
+                                "Camera selection coming soon...",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
 
+                pictureDialog.show()
             }
         }
+    }
+
+    // 사용자에게 갤러리에 있는 사진을 사용할 건지에 대한 권한을 물어본다
+    private fun choosePhotoFromGallery(){
+        Dexter.withContext(this@AddHappyPlaceActivity)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .withListener(object: MultiplePermissionsListener{ // object를 써줬으니 ()는 필요 없다
+                override fun onPermissionsChecked
+                            (report: MultiplePermissionsReport?) {
+                    // 권한이 모두 허가 되었는지 체크
+                    if(report!!.areAllPermissionsGranted()){
+                        Toast.makeText(
+                            this@AddHappyPlaceActivity,
+                            "Storage READ/WRITE permission are granted. Now you can select an image from GALLERY",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onPermissionRationaleShouldBeShown
+                            (permissions: MutableList<PermissionRequest>?, token: PermissionToken?) {
+                    // 사용자에게 권한을 요청하는 이유 설명
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread().check()
+
+        // withListener 안에 객체를 넣어줘야 해서 자바로는 MultiplePermissionsListener() { 할일; }로 했지만
+        // 코틀린에서는 object(객체): MultiplePermissionsListener(타입) { 할일 }으로 했다.
+    }
+
+    private fun showRationalDialogForPermissions(){
+        AlertDialog.Builder(this@AddHappyPlaceActivity)
+            .setMessage("It looks like you gave turned off permission required for this feature." +
+                    "It can be enabled under the Application Settings.")
+            .setPositiveButton("GO TO SETTINGS"){ _, _ ->
+                try{
+                    // 앱 설정 화면을 띄우도록 한다.
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel"){dialog, whitch ->
+                dialog.dismiss()
+            }
+            .show()
     }
 
     private fun updateDateInView(){
